789f082353d73d4107d52687447cbccfe20c2aaf
sijinyu <sijinyudev@gmail.com>
2024-11-16 23:45:19 +0900

    remove: depencies check

--- src/analyzer/componentAnalyzer.ts
+++ src/analyzer/componentAnalyzer.ts
@@ -1,9 +1,7 @@
 import { NodePath } from '@babel/traverse';
-import * as t from '@babel/types';
 import { AnalyzerConfig, ComponentAnalysis } from '../types';
 import { getComponentName } from '../utils';
 import { analyzeProps } from '../utils/analyzerUtils';
-import { isHook } from '../utils/astUtils';
 import { analyzeHooks } from './hooks';
 import { calculateComplexity } from './metrics';
 import { analyzeRenderingBehavior } from './renderAnalysis';
@@ -27,31 +25,9 @@ export function analyzeComponent(
     hooks: analyzeHooks(path),
     complexity: calculateComplexity(path),
     renderAnalysis: analyzeRenderingBehavior(path, config),
-    dependencies: analyzeDependencies(path),
-    expensiveNodes: [], // 연산이 비싼 특정 노드를 저장하는 배열 추가
   };
   validateAnalysis(analysis);
   return analysis;
-}
-
-function analyzeDependencies(path: NodePath): string[] {
-  const dependencies = new Set<string>();
-
-  path.traverse({
-    ImportDeclaration(importPath: NodePath<t.ImportDeclaration>) {
-      const source = importPath.node.source.value;
-      if (!source.startsWith('.') && !source.startsWith('/')) {
-        dependencies.add(source);
-      }
-    },
-    CallExpression(callPath: NodePath<t.CallExpression>) {
-      if (isHook(callPath)) {
-        dependencies.add('react');
-      }
-    },
-  });
-
-  return Array.from(dependencies);
 }
 
 function validateAnalysis(analysis: ComponentAnalysis): void {
